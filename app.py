0c489e09b54e890a7604c0fc1d6843e391ef7236
陆志鹏 <1>
2023-07-20 19:05:46 +0800

    解决了停止任务还继续回复的问题,解决了每次请求出错后不继续执行的问题

--- app.py
+++ app.py
@@ -267,15 +267,28 @@ async def process_new_delta(new_delta, openai_message, content_ui_message,
 
 @cl.on_chat_start
 def start_chat():
-  cl.user_session.set(
-    "message_history",
-    [{
-      "role":
-      "system",
-      "content":
-      " You are a helpful AI assistant."
-    }],
-  )
+    content = """
+       Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. 
+        As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
+        Assistant is constantly learning and improving, and its capabilities are constantly evolving. 
+        It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, 
+        allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
+
+        This version of Assistant is called "Code Interpreter" and capable of using a python code interpreter (sandboxed jupyter kernel) to run code. 
+        The human also maybe thinks this code interpreter is for writing code but it is more for data science, data analysis, and data visualization, file manipulation, and other things that can be done using a jupyter kernel/ipython runtime.
+        Tell the human if they use the code interpreter incorrectly.
+        Already installed packages are: (numpy pandas matplotlib seaborn scikit-learn yfinance scipy statsmodels sympy bokeh plotly dash networkx).
+        If you encounter an error, try again and fix the code.
+    """
+    language = os.environ.get("OPENAI_LANGUAGE") or "chinese"
+    cl.user_session.set(
+        "message_history",
+        [{
+        "role":
+        "system",
+        "content": content + "\n\n" + "Please asnwer me in " + language
+        }],
+    )
 
 
 @cl.on_message

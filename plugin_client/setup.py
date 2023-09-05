d4e321178db4d86f6af69d976b88b7e0d868b314
陆志鹏 <1>
2023-09-05 13:31:41 +0800

    add plugin_client

---
+++ plugin_client/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='plugin_client',
+    version='0.1.1',
+    url='',
+    author='Luzhipeng',
+    author_email='402087139@qq.com',
+    description='Description of my package',
+    packages=find_packages(),    
+    install_requires=[],
+)
